81bc255d7fab086ca26da7cadd7d533e96082094
vhenriquebm <vhenriquebm@gmail.com>
2025-04-07 20:01:16 -0300

    Initial Commit

---
+++ MyMovies/MyMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMoviesApp.swift
+//  MyMovies
+//
+//  Created by Vitor Henrique Barreiro Marinho on 07/04/25.
+//
+
+import SwiftUI
+
+@main
+struct MyMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
